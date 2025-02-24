cf448159d358e7e0462c2d107cc7b44ca6720596
jpcuve <gexcuj01i>
2025-02-24 20:23:16 +0100

    .

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import requests
 
 if __name__ == '__main__':
-    res = requests.get('http://127.0.0.1:5000/data-json')
+    res = requests.get('http://127.0.0.1:5000/data')
     if res.ok:
         data = res.json()
         print(data)
