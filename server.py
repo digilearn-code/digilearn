c11435c506bafdf5506141fa835d820c14cfe298
jpcuve <gexcuj01i>
2025-02-24 17:28:23 +0100

    initial

---
+++ server.py
@@ -0,0 +1,57 @@
+import chevron
+from flask import Flask, Response, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World!"
+
+@app.route('/page-html')
+def page_html():
+    return "<html><body>This is an <b>html</b> page</body></html>"
+
+@app.route('/another-page-html')
+def another_page_html():
+    with open('another-page.html') as f:
+        page = f.read()
+    return page
+
+@app.route('/somehow-dynamic-page-html')
+def somehow_dynamic_page_html():
+    # call url http://localhost:5000/somehow-dynamic-page-html?page-name=another-page
+    page_name = request.args.get('page-name')
+    with open(page_name + '.html') as f:
+        page = f.read()
+    return page
+
+@app.route('/dynamic-page-html')
+def dynamic_page_html():
+    person_name = request.args.get('person-name')
+    with open('dynamic-page.html') as f:
+        page = f.read()
+    filled_page = chevron.render(page, {'name': person_name})
+    return filled_page
+
+@app.route('/image-png')
+def image_png():
+    with open('bachelier.png', 'rb') as f:
+        data = f.read()
+    return Response(data, mimetype='image/png')
+
+@app.route('/data-json')
+def data_json():
+    data = {
+        "hello": "world",
+        "age": 4000000000,
+        "address": {
+            "system": "solar",
+            "planet": 3,
+            "radius": 6300000,
+        }
+    }
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run()
